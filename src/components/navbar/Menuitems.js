d3ceb08110af18ce3c36280d88f706a19f2c7d08
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-07-10 16:13:05 +0530

    test: add custom NavBar

---
+++ src/components/navbar/Menuitems.js
@@ -0,0 +1,24 @@
+export const Navitems=[
+    {
+        title:"Home",
+        url:"#",
+        cName:"nav-links",
+    },
+    {
+        title:"Services",
+        url:"#",
+        cName:"nav-links",
+    },
+    {
+        title:"Contact-US",
+        url:"#",
+        cName:"nav-links",
+    },
+    {
+        title:"Sign UP",
+        url:"#",
+        cName:"nav-links-mobile",
+    },
+];
+
+export default Navitems;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Menuitems from "./Menuitems";
+import Button from '../Button'
+
+import "./Navbar.css";
+import AppleIcon from "@material-ui/icons/Apple";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+
+class Navbar extends Component {
+  state = {
+    clicked: false,
+  };
+
+  handleClick = () => {
+      this.setState({
+          clicked:!this.state.clicked,
+      })
+  };
+
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="Navbar-logo">
+          Sachin Supunthaka <AppleIcon style={{marginLeft:0.5}&&{fontSize:24}}/>
+        </h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          {this.state.clicked ? <CloseIcon style={{color:'red'}}/> : <MenuIcon style={{color:'white'}}/>}
+        </div>
+        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+          {Menuitems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button>Sign-UP</Button>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
